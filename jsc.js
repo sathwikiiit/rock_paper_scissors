4ad34c06ad83071a18a983635f5fc2db61f849ab
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-02-24 20:17:49 +0530

    Update jsc.js

--- jsc.js
+++ jsc.js
@@ -3,7 +3,7 @@ var chr = ["Rock", "Paper", "Scissor"]
 function user(choice) {
 	var user_choice = choice;
 	var com_choice =Math.floor(Math.random() * Math.floor(3))
-	var adit = user_choice + com_choice
+	var adit = user_choice + com_choice - 3
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
 		alert("You lose\n" + chr[user_choice/3-1] + "  " +  chr[com_choice]);
