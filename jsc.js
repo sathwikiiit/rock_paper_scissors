d9695b27eaa32fc79bb6d24ca59a12a2ad47119c
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-02-24 19:23:24 +0530

    Update jsc.js

--- jsc.js
+++ jsc.js
@@ -1,12 +1,17 @@
 var ysc = 0, csc = 0;
 var image = document.getElementById("myImage").src="https://dx5683gi1tv0w.cloudfront.net/dtrjyhj9q/image/upload/w_1080,h_1080,c_pad,b_auto/s3/img0be16e8d8";
+window.onload = function() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  var img = document.getElementById("scream");
+};
 function user(choice) {
 	var user_choice = choice;
 	var com_choice = Math.floor(Math.random()*3)
 	var adit = user_choice + com_choice
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
-		alert("You won" + user_choice + com_choice + image);
+		  ctx.drawImage(img, 10, 10);
 	}
 	else if (adit == adit == 1 || adit == 5 || adit == 6) {
 		csc = csc + 1;
