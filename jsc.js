0e4bc6d0a1e2f6319163d211cd9b1bca90a3fdbc
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-02-24 19:28:42 +0530

    Update jsc.js

--- jsc.js
+++ jsc.js
@@ -1,16 +1,15 @@
 var ysc = 0, csc = 0;
-var imag = document.getElementById("myImage").src="https://dx5683gi1tv0w.cloudfront.net/dtrjyhj9q/image/upload/w_1080,h_1080,c_pad,b_auto/s3/img0be16e8d8";
 function user(choice) {
 	var user_choice = choice;
 	var com_choice = Math.floor(Math.random()*3)
 	var adit = user_choice + com_choice
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
-		alert("You lose" + user_choice + com_choice + image);
+		alert("You lose" + user_choice + com_choice);
 	}
 	else if (adit == adit == 1 || adit == 5 || adit == 6) {
 		csc = csc + 1;
-		alert("You lose" + user_choice + com_choice + image);
+		alert("You lose" + user_choice + com_choice );
 	}
 	document.getElementById('ys').innerHTML = 'your score : ' + ysc;
 	document.getElementById('cs').innerHTML = 'com score : ' + csc;
