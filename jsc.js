c22b05db6da3ea9594b00a8331185788ea646b8b
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-02-24 20:13:10 +0530

    Update jsc.js

--- jsc.js
+++ jsc.js
@@ -6,11 +6,11 @@ function user(choice) {
 	var adit = user_choice + com_choice
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
-		alert("You lose\n" + chr[user_choice/3] + "  " +  chr[com_choice]);
+		alert("You lose\n" + chr[user_choice/3-1] + "  " +  chr[com_choice]);
 	}
 	else if (adit == adit == 1 || adit == 5 || adit == 6) {
 		csc = csc + 1;
-		alert("You lose\n" + chr[user_choice] + "  " + chr[com_choice] );
+		alert("You lose\n" + chr[user_choice/3 -1] + "  " + chr[com_choice] );
 	}
 	document.getElementById('ys').innerHTML = 'your score : ' + ysc;
 	document.getElementById('cs').innerHTML = 'com score : ' + csc;
