8962a6bf1c7b2ac2aaa0457c95d8fedc31f004c1
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-02-24 20:02:08 +0530

    Update jsc.js

--- jsc.js
+++ jsc.js
@@ -1,15 +1,16 @@
 var ysc = 0, csc = 0;
+var chr = ["Rock", "Paper", "Scissor"]
 function user(choice) {
 	var user_choice = choice;
 	var com_choice = Math.floor(Math.random()*3)
 	var adit = user_choice + com_choice
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
-		alert("You lose" + user_choice + com_choice);
+		alert("You lose\n" + chr[user_choice] + "  " +  chr[com_choice]);
 	}
 	else if (adit == adit == 1 || adit == 5 || adit == 6) {
 		csc = csc + 1;
-		alert("You lose" + user_choice + com_choice );
+		alert("You lose\n" + chr[user_choice] + "  " + chr[com_choice] );
 	}
 	document.getElementById('ys').innerHTML = 'your score : ' + ysc;
 	document.getElementById('cs').innerHTML = 'com score : ' + csc;
