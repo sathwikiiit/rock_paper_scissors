b081f0250fb96a9603c64f79c6c66e7038aa3d3d
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2021-04-24 19:35:11 +0530

    Change alerts

--- jsc.js
+++ jsc.js
@@ -8,13 +8,11 @@ function user(choice) {
 	if (adit == 2 || adit == 3 || adit == 7) {
 		ysc = ysc + 1;
 		msg = "You Won"
-		alert( msg +"\n" + chr[user_choice/3 - 1] + "  " +  chr[com_choice]);
-	}
+		alert( msg + "\n" + "URs: " + chr[user_choice/3 - 1] + "   PCs: " + chr[com_choice] );}
 	else if (adit == 1 || adit == 5 || adit == 6) {
 		csc = csc + 1;
 		msg = "You Lose"
-		alert( msg +"\n" + chr[user_choice/3 - 1] + "  " + chr[com_choice] );
-	}
+		alert(msg + "\n" + "URs: " + chr[user_choice/3 - 1] + "   PCs: " + chr[com_choice] );}
 	else if (adit == 4|| adit == 0 || adit == 8) {
 		msg = "Balanced"
 		alert(msg + "\n" + "URs: " + chr[user_choice/3 - 1] + "   PCs: " + chr[com_choice] );}
